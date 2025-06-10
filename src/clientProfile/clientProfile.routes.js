da0ad6d7035ae55f14196f24867dcf095bc8f1f5
prealiquez-2020613 <prealiquez-2020613@kinal.edu.gt>
2025-06-10 16:09:43 -0600

    Se agrega rutas de la entidad 'clientProfile'

---
+++ src/clientProfile/clientProfile.routes.js
@@ -0,0 +1,23 @@
+import { Router } from 'express';
+import {
+  createClientProfile,
+  getAllClientProfiles,
+  updateClientProfile,
+  deleteClientProfile
+} from './clientProfile.controller.js';
+
+import { validateJwt, isClient } from '../../middlewares/validate.jwt.js';
+
+const api = Router();
+
+api.post('/create', [validateJwt, isClient], createClientProfile);
+
+api.get('/all', validateJwt, getAllClientProfiles);
+
+api.get('/user/:userId', validateJwt, getAllClientProfiles);
+
+api.put('/update', [validateJwt, isClient], updateClientProfile);
+
+api.delete('/delete', [validateJwt, isClient], deleteClientProfile);
+
+export default api;
