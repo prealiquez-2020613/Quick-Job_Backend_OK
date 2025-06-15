d96476366a4c1b2a2499d5d0673b76031e84b37c
mbautista-2020375 <mbautista-2020375@kinal.edu.gt>
2025-06-15 16:26:59 -0600

    Se agregan validaciones de la entidad 'chat' en 'chat.routes.js'

--- src/chat/chat.routes.js
+++ src/chat/chat.routes.js
@@ -6,11 +6,12 @@ import {
   getChatById
 } from './chat.controller.js';
 import { validateJwt } from '../../middlewares/validate.jwt.js';
+import { createOrGetChatValidator, sendMessageValidator } from '../../helpers/validators.js';
 
 const api = Router();
 
-api.post('/createchat', [validateJwt], createOrGetChat);
-api.post('/message', [validateJwt], sendMessage);
+api.post('/createchat', [ createOrGetChatValidator], createOrGetChat);
+api.post('/message', [ sendMessageValidator], sendMessage);
 api.get('/allchat', [validateJwt], getUserChats);
 api.get('/get/:chatId', [validateJwt], getChatById);
 
