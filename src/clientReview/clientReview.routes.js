4d0fe76fd8762e5be90ce1675789e59cee7395b5
jalvarado-2021353 <jalvarado-2021353@kinal.edu.gt>
2025-06-15 17:11:25 -0600

    Se agregan validaciones de la entidad 'clientReview' en 'workerReview.routes.js

--- src/clientReview/clientReview.routes.js
+++ src/clientReview/clientReview.routes.js
@@ -5,12 +5,16 @@ import {
   deleteClientReview,
   getAllClientReviews
 } from './clientReview.controller.js';
+import {
+  createClientReviewValidator,
+  updateClientReviewValidator
+} from '../../helpers/validators.js';
 import { validateJwt } from '../../middlewares/validate.jwt.js';
 
 const api = Router();
 
-api.post('/createClientReview', [validateJwt], createClientReview);
-api.put('/updateClientReview/:id', [validateJwt], updateClientReview);
+api.post('/createClientReview', [validateJwt, createClientReviewValidator], createClientReview);
+api.put('/updateClientReview/:id', [validateJwt, updateClientReviewValidator], updateClientReview);
 api.delete('/deleteClientReview/:id', [validateJwt], deleteClientReview);
 api.get('/reviews/client/:clientId', [validateJwt], getAllClientReviews);
 
