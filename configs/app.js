f326a9b6fbb106335f551eae308f81ba3a7f3218
prealiquez-2020613 <prealiquez-2020613@kinal.edu.gt>
2025-06-17 18:13:18 -0600

    Se eliminan las entidades 'workerReview' y 'userReview'

--- configs/app.js
+++ configs/app.js
@@ -11,8 +11,6 @@ import paymentRoutes from '../src/payment/payment.routes.js'
 import rechargeRoutes from '../src/recharge/recharge.routes.js'
 import chatRoutes from '../src/chat/chat.routes.js'
 import jobRequestRoutes from '../src/jobRequest/jobRequest.routes.js'
-import clientReviewRoutes from '../src/clientReview/clientReview.routes.js'
-import workerReviewRoutes from '../src/workerReview/workerReview.routes.js'
 import { initializeDatabase } from './initSetup.js'
 
 import jwt from 'jsonwebtoken'
@@ -36,12 +34,10 @@ const routes = (app) => {
   app.use(authRoutes)
   app.use('/v1/user', userRoutes)
   app.use('/v1/category', categoryRoutes)
-  app.use('/v1/clientReview', clientReviewRoutes)
   app.use('/v1/payment', paymentRoutes)
   app.use('/v1/recharge', rechargeRoutes)
   app.use('/v1/chat', chatRoutes)
   app.use('/v1/jobRequest', jobRequestRoutes)
-  app.use('/v1/workerReview', workerReviewRoutes)
 }
 
 export const initServer = async () => {
