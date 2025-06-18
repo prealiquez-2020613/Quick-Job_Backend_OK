cc3f0c9735609e02278a774cbada9d6d92185f7e
prealiquez-2020613 <prealiquez-2020613@kinal.edu.gt>
2025-06-17 18:14:17 -0600

    Se agrega el esquema de la entidad 'Review'

---
+++ src/review/review.model.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+
+const ReviewSchema = new mongoose.Schema({
+  sender: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  receiver: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  rating: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 5
+  },
+  comment: {
+    type: String,
+    maxLength: 300
+  }
+}, { timestamps: true });
+
+export default mongoose.model('Review', ReviewSchema);
